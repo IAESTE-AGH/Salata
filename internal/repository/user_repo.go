5bf9692920cfb764ddcf68b5ac353cf1ebab2834
w3rr0 <konradmateja65@gmail.com>
2025-11-29 19:38:00 +0100

    get more user info in GetAllUsers

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -6,17 +6,33 @@ import (
 )
 
 func GetAllUsers(db *sql.DB) ([]models.User, error) {
-	rows, err := db.Query("SELECT * FROM users")
+	rows, err := db.Query(`SELECT id, first_name, last_name, email, "group" FROM users`)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	users := []models.User{}
+
 	for rows.Next() {
 		var u models.User
-		//rows.Scan(&u.ID, &u.Name, &u.Email, &u.Group)
-		rows.Scan(&u.ID, &u.Name)
+		var first, last, group string
+
+		if err := rows.Scan(
+			&u.ID,
+			&first,
+			&last,
+			&u.Email,
+			&group,
+		); err != nil {
+			return nil, err
+		}
+
+		u.Name.First = first
+		u.Name.Last = last
+		u.Group = models.Group(group)
+
+		users = append(users, u)
 	}
 	return users, nil
 }
