5ad821fc0ea749ddcc98cb014c816c1cc7b60dbd
w3rr0 <konradmateja65@gmail.com>
2025-11-29 19:42:53 +0100

    add GetUsers func (display all users)

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,1 +1,14 @@
 package handlers
+
+import (
+	"encoding/json"
+	"go_server/internal/database"
+	"go_server/internal/repository"
+	"net/http"
+)
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	db := database.DB
+	users, _ := repository.GetAllUsers(db)
+	json.NewEncoder(w).Encode(users)
+}
