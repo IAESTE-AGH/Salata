6548d0b28dbc18717470b4dae6936f445e24002a
w3rr0 <konradmateja65@gmail.com>
2025-11-29 19:41:08 +0100

    add database connection func (Init)

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -1,1 +1,23 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func Init(connString string) {
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal("Error connecting to database:", err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal("Cannot connect to database:", err)
+	}
+
+	DB = db
+	log.Println("Connected to database")
+}
