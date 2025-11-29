1e85d0d69a80fa1ebffe1cec7bea2b960e102569
w3rr0 <konradmateja65@gmail.com>
2025-11-29 18:16:11 +0100

    add GetAllUsers func to user_repo

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"database/sql"
+	"go_server/internal/models"
+)
+
+func GetAllUsers(db *sql.DB) ([]models.User, error) {
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users := []models.User{}
+	for rows.Next() {
+		var u models.User
+		//rows.Scan(&u.ID, &u.Name, &u.Email, &u.Group)
+		rows.Scan(&u.ID, &u.Name)
+	}
+	return users, nil
+}
