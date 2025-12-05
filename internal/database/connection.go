3d6339f5d055f305fff6ec20d150239323c87a5e
w3rr0 <konradmateja65@gmail.com>
2025-12-05 23:48:58 +0100

    change logic in connection init to retry connection to db up to 5 times

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -10,14 +11,24 @@ import (
 var DB *sql.DB
 
 func Init(connString string) {
-	db, err := sql.Open("postgres", connString)
-	if err != nil {
-		log.Fatal("Error connecting to database:", err)
+	var db *sql.DB
+	var err error
+
+	for i := 0; i < 5; i++ {
+		db, err = sql.Open("postgres", connString)
+		if err != nil {
+			log.Fatal("Error connecting to database:", err)
+		}
+		if err = db.Ping(); err == nil {
+			DB = db
+			log.Println("Successfully connected to database")
+			break
+		} else {
+			log.Printf("Database not ready... attempt %d/5. Waiting 2 seconds...\n", i+1)
+			time.Sleep(2 * time.Second)
+		}
 	}
-	if err := db.Ping(); err != nil {
+	if err != nil {
 		log.Fatal("Cannot connect to database:", err)
 	}
-
-	DB = db
-	log.Println("Connected to database")
 }
