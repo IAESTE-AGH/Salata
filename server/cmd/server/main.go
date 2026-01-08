3757a86f206a8cc1a3256a72e5ec1bac285f4566
w3rr0 <konradmateja65@gmail.com>
2026-01-08 17:01:31 +0100

    add middleware to private endpoints

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"go_server/internal/config"
 	"go_server/internal/database"
 	"go_server/internal/handlers"
+	"go_server/middleware"
 	"log"
 	"net/http"
 )
@@ -12,15 +13,24 @@ func main() {
 	database.Init(config.DatabaseURL)
 
 	mux := http.NewServeMux()
+
+	protect := func(handlerFunc http.HandlerFunc) http.Handler {
+		handler := http.HandlerFunc(handlerFunc)
+		return middleware.AuthMiddleware(handler)
+	}
+
+	// Public (does not require authentication)
 	mux.HandleFunc("/status", handlers.HandleGetStatus)
 	mux.HandleFunc("/users", handlers.HandleGetUsers)
-	mux.HandleFunc("/create_event", handlers.HandleCreateEvent)
-	mux.HandleFunc("/delete_event", handlers.HandleDeleteEvent)
-	mux.HandleFunc("/change_availability", handlers.HandleChangeAvailability)
-	mux.HandleFunc("/get_all_current_events", handlers.HandleGetAllCurrentEvents)
-	mux.HandleFunc("/create_user", handlers.HandleCreateUser)
-	mux.HandleFunc("/verify_user", handlers.HandleVerifyUser)
 	mux.HandleFunc("/login", handlers.HandleLogin)
+	mux.HandleFunc("/verify_user", handlers.HandleVerifyUser)
+	mux.HandleFunc("/create_user", handlers.HandleCreateUser)
+
+	// Private (requires authentication)
+	mux.Handle("/create_event", protect(handlers.HandleCreateEvent))
+	mux.Handle("/delete_event", protect(handlers.HandleDeleteEvent))
+	mux.Handle("/change_availability", protect(handlers.HandleChangeAvailability))
+	mux.Handle("/get_all_current_events", protect(handlers.HandleGetAllCurrentEvents))
 
 	log.Println("Server is running at :8080")
 	err := http.ListenAndServe(":8080", mux)
