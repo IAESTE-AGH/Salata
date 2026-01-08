e668e88f9fd9fff2e5e7a2c4c8d8e9fcce569f1b
w3rr0 <konradmateja65@gmail.com>
2026-01-08 16:22:32 +0100

    create middleware auth func

---
+++ server/middleware/auth.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+type contextKey string
+
+const UserIDKey contextKey = "userID"
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "No auth token provided", http.StatusUnauthorized)
+			return
+		}
+	})
+}
