537f3f5d9bbb4b57d53c9b05cc6b4ba12d849c5d
w3rr0 <konradmateja65@gmail.com>
2026-01-08 23:53:42 +0100

    move create auth user to separated func

--- server/tests/basic_flow_test.go
+++ server/tests/basic_flow_test.go
@@ -2,49 +2,64 @@ package tests
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	"go_server/internal/config"
 	"go_server/internal/repository"
+	"go_server/middleware"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func TestLoginFlow(t *testing.T) {
-	testDB := setupTestDB(t)
-	router := setupRouter()
-
-	email := "test.email@iaeste.pl"
+func createAuthenticatedUser(t *testing.T, db *sql.DB) (string, string, string) {
+	email := fmt.Sprintf("user.%d@iaeste.pl", time.Now().UnixNano())
 	password := "testpassword"
 	firstName, lastName, err := repository.ExtractNameFromEmail(email)
 	if err != nil {
 		t.Fatalf("Invalid email format: %s", err)
 	}
 
-	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		t.Fatalf("Hash error: %v", err)
-	}
-	passwordHash := string(hashedBytes)
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
-	_, err = testDB.Exec(`
-		INSERT INTO accounts (email, password_hash, verification_token, is_verified)
-		VALUES ($1, $2, '', TRUE)`,
-		email, passwordHash,
-	)
+	_, err = db.Exec(`INSERT INTO accounts (email, password_hash, verification_token, is_verified)
+		VALUES ($1, $2, '', TRUE)`, email, string(hashed))
 	if err != nil {
-		t.Fatalf("Error inserting into accounts: %v", err)
+		t.Fatalf("Helper: Błąd insert accounts: %v", err)
 	}
 
-	_, err = testDB.Exec(`
-		INSERT INTO users (first_name, last_name, email, "group")
-		VALUES ($1, $2, $3, 'IT')`,
-		firstName, lastName, email,
-	)
+	var userID int
+	err = db.QueryRow(`INSERT INTO users (first_name, last_name, email, "group")
+		VALUES ($2, $3, $1, 'IT') RETURNING id`, email, firstName, lastName).Scan(&userID)
 	if err != nil {
-		t.Fatalf("Error inserting into users: %v", err)
+		t.Fatalf("Helper: Błąd insert users: %v", err)
 	}
+
+	claims := &middleware.Claims{
+		UserId: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
+		},
+	}
+	tokenObj := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := tokenObj.SignedString([]byte(config.JWTSecret))
+	if err != nil {
+		t.Fatalf("Helper: Błąd podpisywania tokena: %v", err)
+	}
+
+	return tokenString, email, password
+}
+
+func TestLoginFlow(t *testing.T) {
+	testDB := setupTestDB(t)
+	router := setupRouter()
+
+	token, email, password := createAuthenticatedUser(t, testDB)
 
 	loginPayload, _ := json.Marshal(map[string]string{
 		"email":    email,
