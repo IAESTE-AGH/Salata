98d22f178c50de3d5d73f24ed964d2e9fb86295f
w3rr0 <konradmateja65@gmail.com>
2026-01-08 19:05:10 +0100

    load user id from context in change availability

--- server/internal/handlers/change_availability.go
+++ server/internal/handlers/change_availability.go
@@ -4,17 +4,22 @@ import (
 	"encoding/json"
 	"go_server/internal/database"
 	"go_server/internal/repository"
+	"go_server/middleware"
 	"net/http"
 )
 
 type RequestChangeAvailability struct {
 	EventId      int               `json:"event_id"`
-	UserId       int               `json:"user_id"`
 	Availability map[string]string `json:"availability"`
 }
 
 func HandleChangeAvailability(w http.ResponseWriter, r *http.Request) {
-	db := database.DB
+	userID, ok := middleware.GetUserID(r.Context())
+	if !ok {
+		http.Error(w, "User ID not found in context", http.StatusUnauthorized)
+		return
+	}
+
 	var req RequestChangeAvailability
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -22,9 +27,14 @@ func HandleChangeAvailability(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = repository.ChangeAvailability(db, req.EventId, req.UserId, req.Availability)
+	db := database.DB
+
+	err = repository.ChangeAvailability(db, req.EventId, userID, req.Availability)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Availability updated"))
 }
